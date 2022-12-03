cceefc9b146db444ee62fd88924477bf4a2ad6fe
saehim <saeilee.vxv@gmail.com>
2022-12-03 14:09:55 +0900

    feat (controller) : 게임 생성 메서드

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -8,27 +8,33 @@ import bridge.view.GameMessage;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
+import java.util.List;
+
 public class BridgeGameController {
 
     private final InputView inputView;
     private final OutputView outputView;
-    private final BridgeGame bridgeGame;
+    private BridgeGame bridgeGame;
     private final BridgeMaker bridgeMaker;
 
     public BridgeGameController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
-        this.bridgeGame = new BridgeGame();
         this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     }
 
     /**
      * 게임 생성
      * */
-    public void initGame () {
+    public void startGame () {
         Printer.print(GameMessage.START_MESSAGE.getMessage());
+        initGame();
+    }
+
+    private void initGame() {
         int size = inputView.readBridgeSize();
-        bridgeMaker.makeBridge(size);
+        List<String> bridge = bridgeMaker.makeBridge(size);
+        bridgeGame = new BridgeGame(bridge);
     }
 
     /**
