194d9b3794dab4fde70304ab5b90e1e1b75c889a
saehim <saeilee.vxv@gmail.com>
2022-12-03 12:54:19 +0900

    feat (exception) : 입력값이 숫자가 아닌경우 예외 메시지

--- src/main/java/bridge/exception/Exception.java
+++ src/main/java/bridge/exception/Exception.java
@@ -1,7 +1,8 @@
 package bridge.exception;
 
 public enum Exception {
-    NOT_VAILD_SIZE("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    NOT_VAILD_SIZE("다리 길이는 3부터 20 사이의 숫자여야 합니다."),
+    NOT_NUMBER("숫자를 입력해 주세요.");
 
     private final String message;
     private final String tag = "[ERROR] ";
