d1b967c90e72ef878677198659640e985ce94054
saehim <saeilee.vxv@gmail.com>
2022-12-03 11:59:56 +0900

    feat (bridge) : 랜덤 다리 생성 메서드 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,9 @@
 package bridge;
 
+import bridge.domain.Bridge;
+import bridge.exception.Exception;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,6 +12,10 @@ import java.util.List;
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
+
+    private final int MIN_BRIDGE_SIZE = 3;
+    private final int MAX_BRIDGE_SIZE = 20;
+
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
@@ -18,6 +26,22 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        isVaildSize(size);
+
+        List<String> bridge = new ArrayList<>();
+
+        for (int i=0; i<size; i++) {
+            int index = bridgeNumberGenerator.generate();
+            bridge.add(Bridge.getPathCommand(index));
+        }
+
+        return bridge;
     }
+
+    private void isVaildSize(int size) {
+        if (MIN_BRIDGE_SIZE>size || size>MAX_BRIDGE_SIZE) {
+            throw new IllegalArgumentException(Exception.NOT_VAILD_SIZE.getMessage());
+        }
+    }
+
 }
