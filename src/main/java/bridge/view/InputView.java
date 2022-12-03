729d4abad10b016a918477d01cdb0b2bee992d5c
saehim <saeilee.vxv@gmail.com>
2022-12-03 12:52:02 +0900

    feat (view) : 사용자 다리 길이 묻는 메서드

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,15 +1,42 @@
 package bridge.view;
 
+import bridge.exception.Exception;
+import bridge.util.Printer;
+
+import java.util.regex.Pattern;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private final String INT_PATTERN = "^[0-9]+$";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        Printer.print(GameMessage.ASK_BRIDGE_SIZE.getMessage());
+        return readInt();
+    }
+
+    private int readInt() {
+        try {
+            String s = readLine();
+            isVaildNumberFormat(s);
+            return Integer.parseInt(s);
+        } catch (IllegalArgumentException e) {
+            Printer.print(e.getMessage());
+            return readBridgeSize();
+        }
+    }
+
+    private void isVaildNumberFormat(String s) {
+        if (!s.matches(INT_PATTERN)) {
+            throw new IllegalArgumentException(Exception.NOT_NUMBER.getMessage());
+        }
     }
 
     /**
@@ -25,4 +52,6 @@ public class InputView {
     public String readGameCommand() {
         return null;
     }
+
+
 }
