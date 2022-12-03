c6df260b66b2ecc21094402f3535a73de420cdcf
saehim <saeilee.vxv@gmail.com>
2022-12-03 15:30:16 +0900

    feat (BridgeGame) : 라운드 시행

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -29,19 +29,37 @@ public class BridgeGameController {
     public void startGame () {
         Printer.print(GameMessage.START_MESSAGE.getMessage());
         initGame();
+
+        while (!bridgeGame.isGoal()) {
+            goForward();
+        }
     }
 
+
+    /**
+     * 게임 시작
+     * */
     private void initGame() {
         int size = inputView.readBridgeSize();
         List<String> bridge = bridgeMaker.makeBridge(size);
         bridgeGame = new BridgeGame(bridge);
     }
 
-    /**
-     * 게임 시작
-     * */
 
     /**
      * 라운드 플레이
      * */
+    private void goForward() {
+        String move = inputView.readMoving();
+        boolean checkStep =  bridgeGame.move(move);
+        outputView.printMap(bridgeGame.getPlayBridge(), checkStep);
+        if (!checkStep) {
+            failGame();
+        }
+    }
+
+    private void failGame() {
+        Printer.print();
+    }
+
 }
