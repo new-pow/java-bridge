a62d556a7af653906535e029ced98495ed6987ed
saehim <saeilee.vxv@gmail.com>
2022-12-03 12:52:47 +0900

    test (view) : 사용자 다리 길이 묻는 메서드
    - 숫자가 아닌 경우 예외 테스트
    - 기능테스트

--- src/test/java/bridge/controller/BridgeGameControllerTest.java
+++ src/test/java/bridge/controller/BridgeGameControllerTest.java
@@ -1,4 +1,33 @@
+package bridge.controller;
+
+import bridge.exception.Exception;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+
 class BridgeGameControllerTest {
-  
+    private static BridgeGameController bridgeGameController;
+
+   @BeforeEach
+   private void Befor() {
+       bridgeGameController = new BridgeGameController();
+   }
+
+    @Test
+    void 게임생성테스트_숫자아닐때() {
+        String input = "%";
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertThatThrownBy(() -> bridgeGameController.initGame())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Exception.NOT_NUMBER.getMessage());
+    }
 }
--- src/test/java/bridge/view/InputViewTest.java
+++ src/test/java/bridge/view/InputViewTest.java
@@ -1,4 +1,20 @@
+package bridge.view;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.junit.jupiter.api.Assertions.*;
-class InputViewTest {
-  
+
+class InputViewTest{
+    @Test
+    void readIntTest() {
+        String input = "5";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        InputView inputView = new InputView();
+        inputView.readBridgeSize();
+    }
 }
