f7014c6ad62cd624f898ca658c7cbdbe57dc30be
saehim <saeilee.vxv@gmail.com>
2022-12-03 14:33:15 +0900

    feat (judge) : 다리 위치에 따라 정답 체크 메서드

--- src/test/java/bridge/service/JudgeTest.java
+++ src/test/java/bridge/service/JudgeTest.java
@@ -1,4 +1,34 @@
+package bridge.service;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class JudgeTest {
-  
+
+    @Test
+    void 기능테스트_맞을때() {
+        List<String> bridge = Arrays.asList("U","D","U");
+        Judge judge = new Judge(bridge);
+
+        int step = 0;
+        String move = "U";
+
+        assertThat(judge.checkStep(step,move)).isTrue();
+    }
+
+    @Test
+    void 기능테스트_틀릴때() {
+        List<String> bridge = Arrays.asList("U","D","U");
+        Judge judge = new Judge(bridge);
+
+        int step = 2;
+        String move = "D";
+
+        assertThat(judge.checkStep(step,move)).isFalse();
+    }
 }
